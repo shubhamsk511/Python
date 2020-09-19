cfe522a4654ad54a0787ef15c1c692ffbe623379
shubhamsk511 <71395760+shubhamsk511@users.noreply.github.com>
2020-09-19 16:58:00 +0530

    Add files via upload

---
+++ Assignment 02.py
@@ -0,0 +1,40 @@
+#assignment 2
+#1.operands, an expression
+#2)	Write a program to print the quotient when 9 is divided by 2.
+#print(9//2)
+
+#3)	Write a program to print the remainder when 22 is divided by 3.
+#print(22%3)
+
+#4)	Write a program to create two variables, store an integer value in them and run the arithmetic operations on them. Print the answers.
+# a=4
+# b=8
+# print(a+b)
+
+# 5)Write a program to create two variables, store an integer value in them and run the comparison/relational operations on them. Print the answers.
+# a=5
+# b=8
+# if a<b:
+#     print('true')
+
+# Q6) is it suppose to solve with logical operators or anything else. I am bit a confused
+# 7)	Write a program to create two variables. Assign any value to them using assig operator. Once the values are assigned perform the assignment operations on them.
+# a=8
+# b=4
+# a+=10
+# b*=5
+# print(a,b)
+# 8)	How can one check if 10 is greater than 0 and less than 20? Write a program for the same.
+# a=10
+# if a>0 and a<20:
+#     print('true')
+
+#q9)
+# list1=['India','raj','apple']
+# list2=['India','shubham','mango']
+# list3=['india','om','kiwi']
+# a=list1
+# if a is list1:
+#     print('same')
+#     if 'India' in list1:
+#         print('yes it is')
