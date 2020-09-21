1fb8f171520445dff11b1af04e9cf70f63adb35f
shubhamsk511 <71395760+shubhamsk511@users.noreply.github.com>
2020-09-21 11:55:46 +0530

    Add files via upload

---
+++ Assignment 3.py
@@ -0,0 +1,18 @@
+#  Q2)a = 5
+# b = 8
+# if a > b:
+#     print('a is samll')
+# elif a < b:
+#     print('a is big')
+# else:
+#     print('a and b is same')
+#Q3
+# list=['raj:18','om:16','owl:21']
+
+
+# Q5
+# marry= input('is it raining:')
+# if marry== "yes":
+#     print('eat ice cream')
+# else:
+#     print('eat chochlate')
