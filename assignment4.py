a1ff590e9b3167af1c76a980bdaa0442a8bb427f
shubhamsk511 <71395760+shubhamsk511@users.noreply.github.com>
2020-09-25 16:19:48 +0530

    Add files via upload

---
+++ assignment4.py
@@ -0,0 +1,38 @@
+# #q 1)
+# # a = ['india','pakistan','australia','china','america','georgia','ireland','iceland','geenland']
+# # for x in a:
+# #     print(x)
+# #q 2)
+# a = ['1','2','3','4','5','6','7','8','9','10']
+# for x in a:
+#
+#q3)
+# a = []
+# b = []
+# for x in range(1,101):
+#     if (x%2==0):
+#         a.append(x)
+#         print(a)
+#     elif (x%2!=0):
+#        b.append(x)
+#        print(b)
+
+# q4)
+a=[]
+b=[]
+c=[]
+d=[]
+e=[]
+for x in range (1,101):
+    if (x%2==0):
+        a.append(x)
+        print (a)
+    elif (x%3==0):
+        b.append(x)
+        print(b)
+    elif (x%4==0):
+        c.append(x)
+        print(c)
+    elif (x%5==0):
+        d.append(x)
+        print(d)
